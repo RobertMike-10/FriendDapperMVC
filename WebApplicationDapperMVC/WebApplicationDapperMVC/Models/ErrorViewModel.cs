776f2b9755b920334597d1c189a15c6949b28d8f
Roberto Miguel García Aguirre <61521501+RobertMike-10@users.noreply.github.com>
2021-12-22 09:56:01 -0600

    Initial commit

---
+++ WebApplicationDapperMVC/WebApplicationDapperMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationDapperMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
